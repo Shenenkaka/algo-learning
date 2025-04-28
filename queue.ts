cf1d2b978cf36e966f4b85c235d82f54c464461b
zhonghua <wuzhonghua@yibaiqin.com>
2025-04-28 16:49:54 +0800

    initial read

---
+++ queue.ts
@@ -0,0 +1,65 @@
+export default class Queue {
+  nums: number[];
+  front = 0;
+  queueSize = 0;
+
+  constructor(capacity: number) {
+    this.nums = new Array(capacity);
+  }
+
+  get size() {
+    return this.queueSize;
+  }
+
+  get capacity() {
+    return this.nums.length;
+  }
+
+  isEmpty() {
+    return this.queueSize === 0;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      throw new Error("队列为空");
+    }
+    return this.nums[this.front];
+  }
+
+  push(el: number) {
+    if (this.queueSize === this.capacity) {
+      console.log("队列已满");
+      return;
+    }
+    const rear = (this.front + this.queueSize) % this.capacity;
+    this.nums[rear] = el;
+    this.queueSize++;
+  }
+
+  pop() {
+    const num = this.peek();
+    this.front = (this.front + 1) % this.capacity;
+    this.queueSize--;
+    return num;
+  }
+
+  toArray() {
+    const arr = new Array(this.size);
+    for (let i = 0, j = this.front; i < this.size; i++, j++) {
+      arr[i] = this.nums[j % this.capacity];
+    }
+    return arr
+  }
+}
+
+
+const q =  new Queue(5)
+q.push(1)
+q.push(2)
+q.push(3)
+q.push(4)
+q.push(5)
+q.pop()
+q.push(6)
+console.log('queue: ', q.toArray(), q.front)
+
